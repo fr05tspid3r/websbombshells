4e488fdc4daf0a3a1b7ff1be8a49ba81485ed6ff
fr05tspid3r <145811754+fr05tspid3r@users.noreply.github.com>
2023-09-28 11:57:54 -0400

    Update link_finder.py

--- link_finder.py
+++ link_finder.py
@@ -25,6 +25,4 @@ class LinkFinder(HTMLParser):
 #finder.feed('<html><head><title>Test</title></head>''<body><h1>Parse me!<h1></body></html>')
 
 
-#2:36 caps are basics now were getting into custom fuctionality and i just want to show you how this works, there is a method in HTML parser thats called handle start tag. this method will
-#be called when ever it comes across a start tag. <p> this would be the start tag </p> this would be the end tag. you can use it by default.
 
